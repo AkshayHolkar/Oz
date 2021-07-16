064da1acf8809294f89ed023e77ad2462499c52a
Akshay <dhamakk@gmail.com>
2021-07-16 13:50:56 +1000

    feat: add share service

---
+++ Oz/Services/ISharedService.cs
@@ -0,0 +1,13 @@
+﻿using Oz.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Services
+{
+    public interface ISharedService
+    {
+        bool UserOwnsDomain(IAuthorization domain, string userId);
+    }
+}
---
+++ Oz/Services/SharedService.cs
@@ -0,0 +1,16 @@
+﻿using Oz.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Services
+{
+    public class SharedService : ISharedService
+    {
+        public bool UserOwnsDomain(IAuthorization domain, string userId)
+        {
+            return domain.CustomerId == userId;
+        }
+    }
+}
