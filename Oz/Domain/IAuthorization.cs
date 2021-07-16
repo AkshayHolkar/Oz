237429c851a44c9949b44d80841e6d8e00e90384
Akshay <dhamakk@gmail.com>
2021-07-16 13:54:36 +1000

    feat: add authorization interface

---
+++ Oz/Domain/IAuthorization.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Domain
+{
+    public interface IAuthorization
+    {
+        string CustomerId { get; set; }
+    }
+}
