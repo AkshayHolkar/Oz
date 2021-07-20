b95cc74ce3c83eec0e7212dcf9abde40c4790072
AkshayHolkar <dhamakk@gmail.com>
2021-07-21 08:12:27 +1000

    feat: create account dtos

---
+++ Oz/Dtos/AccountDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Dtos
+{
+    public class AccountDto
+    {
+        public string UserId { get; set; }
+        public string ContactName { get; set; }
+        public string BusinessName { get; set; }
+        public int ABN { get; set; }
+        public string Phone { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Postcode { get; set; }
+        public string Country { get; set; }
+        public bool Approved { get; set; } = false;
+    }
+}
---
+++ Oz/Dtos/CreateAccountDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Dtos
+{
+    public class CreateAccountDto
+    {
+        [Required]
+        public string ContactName { get; set; }
+        [Required]
+        public string BusinessName { get; set; }
+        [Required]
+        public int ABN { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public int Postcode { get; set; }
+    }
+}
