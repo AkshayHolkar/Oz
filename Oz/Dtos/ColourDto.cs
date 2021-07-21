7cb80311469ec8bed7933537fc861eb090bd6786
AkshayHolkar <dhamakk@gmail.com>
2021-07-21 13:15:12 +1000

    feat: create colour dtos

---
+++ Oz/Dtos/ColourDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Dtos
+{
+    public class ColourDto
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string Color { get; set; }
+        public int ProductQuantity { get; set; }
+    }
+}
---
+++ Oz/Dtos/PostColourDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Dtos
+{
+    public class PostColourDto
+    {
+        public int ProductId { get; set; }
+        public string Color { get; set; }
+        public int ProductQuantity { get; set; }
+    }
+}
