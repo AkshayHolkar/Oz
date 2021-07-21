1fff77e7ec4183f131194e5d3779fea0a8b4e71f
AkshayHolkar <dhamakk@gmail.com>
2021-07-22 09:03:22 +1000

    feat: add productSize dtos extension

---
+++ Oz/Extensions/ProductSizeDtosExtension.cs
@@ -0,0 +1,41 @@
+﻿using Oz.Domain;
+using Oz.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Extensions
+{
+    public static class ProductSizeDtosExtension
+    {
+        public static ProductSizeDto AsDto(this ProductSize productSize)
+        {
+            return new ProductSizeDto
+            {
+                Id = productSize.Id,
+                ProductId = productSize.ProductId,
+                ItemSize = productSize.ItemSize
+            };
+        }
+
+        public static ProductSize AsProductSizeFromProductSizeDto(this ProductSizeDto productSize)
+        {
+            return new ProductSize
+            {
+                Id = productSize.Id,
+                ProductId = productSize.ProductId,
+                ItemSize = productSize.ItemSize
+            };
+        }
+
+        public static ProductSize AsProductSizeFromPostProductSizeDto(this PostProductSizeDto productSize)
+        {
+            return new ProductSize
+            {
+                ProductId = productSize.ProductId,
+                ItemSize = productSize.ItemSize
+            };
+        }
+    }
+}
