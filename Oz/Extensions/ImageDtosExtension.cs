963be3241ecc19537d626b3218a630478d6a8512
AkshayHolkar <dhamakk@gmail.com>
2021-07-21 13:57:17 +1000

    feat: add image dtos extension

---
+++ Oz/Extensions/ImageDtosExtension.cs
@@ -0,0 +1,35 @@
+﻿using Oz.Domain;
+using Oz.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Extensions
+{
+    public static class ImageDtosExtension
+    {
+        public static ImageDto AsDto(this Image image)
+        {
+            return new ImageDto
+            {
+                Id = image.Id,
+                Name = image.Name,
+                Main = image.Main,
+                ProductId = image.ProductId,
+                ImageScr = image.ImageScr
+            };
+        }
+
+        public static Image AsImageFromPutImageDto(this PutImageDto image)
+        {
+            return new Image
+            {
+                Id = image.Id,
+                Name = image.Name,
+                Main = image.Main,
+                ProductId = image.ProductId,
+            };
+        }
+    }
+}
