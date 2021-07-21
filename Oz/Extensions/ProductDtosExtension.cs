1f1a531633c84decc9d0ea685acc478d176cb598
AkshayHolkar <dhamakk@gmail.com>
2021-07-22 08:38:02 +1000

    feat: add product dtos extension

---
+++ Oz/Extensions/ProductDtosExtension.cs
@@ -0,0 +1,55 @@
+﻿using Oz.Domain;
+using Oz.Dtos;
+
+namespace Oz.Extensions
+{
+    public static class ProductDtosExtension
+    {
+        public static ProductDto AsDto(this Product product)
+        {
+            return new ProductDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Quantity = product.Quantity,
+                Description = product.Description,
+                Status = product.Status,
+                CategoryId = product.CategoryId,
+                SizeNotApplicable = product.SizeNotApplicable,
+                ColorNotApplicable = product.ColorNotApplicable
+            };
+        }
+
+        public static Product AsProductFromProductDto(this ProductDto product)
+        {
+            return new Product
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Quantity = product.Quantity,
+                Description = product.Description,
+                Status = product.Status,
+                CategoryId = product.CategoryId,
+                SizeNotApplicable = product.SizeNotApplicable,
+                ColorNotApplicable = product.ColorNotApplicable
+            };
+        }
+
+        public static Product AsProductFromPostProductDto(this PostProductDto product)
+        {
+            return new Product
+            {
+                Name = product.Name,
+                Price = product.Price,
+                Quantity = product.Quantity,
+                Description = product.Description,
+                Status = product.Status,
+                CategoryId = product.CategoryId,
+                SizeNotApplicable = product.SizeNotApplicable,
+                ColorNotApplicable = product.ColorNotApplicable
+            };
+        }
+    }
+}
