d081c9db405198f5af39ac5838e8f1123d6102c0
AkshayHolkar <dhamakk@gmail.com>
2021-07-21 18:27:25 +1000

    feat: create order dtos

---
+++ Oz/Dtos/OrderDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Dtos
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public DateTime DateCreation { get; set; }
+        public string CustomerId { get; set; }
+        public string OrderStatus { get; set; }
+    }
+}
---
+++ Oz/Dtos/SingleOrderDto.cs
@@ -0,0 +1,17 @@
+﻿using Oz.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Dtos
+{
+    public class SingleOrderDto
+    {
+        public int Id { get; set; }
+        public DateTime DateCreation { get; set; }
+        public string CustomerId { get; set; }
+        public string OrderStatus { get; set; }
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
