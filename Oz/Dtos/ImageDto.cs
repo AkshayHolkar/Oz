dca0a34e31ab8fa622167cee7b88e9f4c1a33b8c
AkshayHolkar <dhamakk@gmail.com>
2021-07-21 13:54:41 +1000

    feat: create image dtos

---
+++ Oz/Dtos/ImageDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Dtos
+{
+    public class ImageDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Main { get; set; }
+        public int ProductId { get; set; }
+        public string ImageScr { get; set; }
+    }
+}
---
+++ Oz/Dtos/PutImageDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Dtos
+{
+    public class PutImageDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Main { get; set; }
+        public int ProductId { get; set; }
+    }
+}
