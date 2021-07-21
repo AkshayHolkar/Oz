655b3f3e8cd9ba95db4f655d67f3f783ea7deb2d
AkshayHolkar <dhamakk@gmail.com>
2021-07-22 08:37:04 +1000

    feat: create product dtos

---
+++ Oz/Dtos/PostProductDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Oz.Dtos
+{
+    public class PostProductDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public bool Status { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [Required]
+        public bool SizeNotApplicable { get; set; }
+        [Required]
+        public bool ColorNotApplicable { get; set; }
+    }
+}
---
+++ Oz/Dtos/ProductDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Oz.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+        public int CategoryId { get; set; }
+        public bool SizeNotApplicable { get; set; }
+        public bool ColorNotApplicable { get; set; }
+    }
+}
