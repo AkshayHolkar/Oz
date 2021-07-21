366d48d9d6ea3d9e52802ab675c0a284aa077214
AkshayHolkar <dhamakk@gmail.com>
2021-07-22 06:15:30 +1000

    feat: add order detail dtos extension

---
+++ Oz/Extensions/OrderDetailDtosExtension.cs
@@ -0,0 +1,52 @@
+﻿using Oz.Domain;
+using Oz.Dtos;
+
+namespace Oz.Extensions
+{
+    public static class OrderDetailDtosExtension
+    {
+        public static OrderDetailDto AsDto(this OrderDetail orderDetail)
+        {
+            return new OrderDetailDto
+            {
+                Id = orderDetail.Id,
+                OrderId = orderDetail.OrderId,
+                ProductId = orderDetail.ProductId,
+                ProductName = orderDetail.ProductName,
+                Color = orderDetail.Color,
+                Size = orderDetail.Size,
+                TotalPrice = orderDetail.TotalPrice,
+                Quantity = orderDetail.Quantity
+            };
+        }
+
+        public static OrderDetail AsOrderFromOrderDetailDto(this OrderDetailDto orderDetail)
+        {
+            return new OrderDetail()
+            {
+                Id = orderDetail.Id,
+                OrderId = orderDetail.OrderId,
+                ProductId = orderDetail.ProductId,
+                ProductName = orderDetail.ProductName,
+                Color = orderDetail.Color,
+                Size = orderDetail.Size,
+                TotalPrice = orderDetail.TotalPrice,
+                Quantity = orderDetail.Quantity
+            };
+        }
+
+        public static OrderDetail AsOrderFromPostOrderDetailDto(this PostOrderDetailDto orderDetail)
+        {
+            return new OrderDetail()
+            {
+                OrderId = orderDetail.OrderId,
+                ProductId = orderDetail.ProductId,
+                ProductName = orderDetail.ProductName,
+                Color = orderDetail.Color,
+                Size = orderDetail.Size,
+                TotalPrice = orderDetail.TotalPrice,
+                Quantity = orderDetail.Quantity
+            };
+        }
+    }
+}
