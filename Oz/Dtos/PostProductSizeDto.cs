01d4d08a34671cfa48b6bc81367f397277ccee5c
AkshayHolkar <dhamakk@gmail.com>
2021-07-22 09:02:04 +1000

    feat: create productSize dtos

---
+++ Oz/Dtos/PostProductSizeDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Oz.Dtos
+{
+    public class PostProductSizeDto
+    {
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public string ItemSize { get; set; }
+    }
+}
---
+++ Oz/Dtos/ProductSizeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Oz.Dtos
+{
+    public class ProductSizeDto
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ItemSize { get; set; }
+    }
+}
