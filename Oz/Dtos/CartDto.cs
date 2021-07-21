9059ff082dcf8ce6da78e9e4e49d9a2b6b2b15f1
AkshayHolkar <dhamakk@gmail.com>
2021-07-21 12:37:59 +1000

    feat: create cart dtos

---
+++ Oz/Dtos/CartDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Dtos
+{
+    public class CartDto
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string CustomerId { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public int MaxLimit { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
---
+++ Oz/Dtos/CreateCartDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oz.Dtos
+{
+    public class CreateCartDto
+    {
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public int MaxLimit { get; set; }
+        [Required]
+        public double Price { get; set; }
+
+    }
+}
