3bb3ec5be62d8336e9535759412253a387963dc1
AkshayHolkar <dhamakk@gmail.com>
2021-07-22 06:13:47 +1000

    feat: create order detail dtos

---
+++ Oz/Dtos/OrderDetailDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Oz.Dtos
+{
+    public class OrderDetailDto
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public double TotalPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Oz/Dtos/PostOrderDetailDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Oz.Dtos
+{
+    public class PostOrderDetailDto
+    {
+        [Required]
+        public int OrderId { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        [Required]
+        public double TotalPrice { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
