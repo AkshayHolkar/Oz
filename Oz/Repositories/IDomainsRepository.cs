677ecd574b5728eb8c935726f9e497fbc3008c44
AkshayHolkar <dhamakk@gmail.com>
2021-07-22 12:20:32 +1000

    feat: add domains repository interface

---
+++ Oz/Repositories/IDomainsRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Oz.Repositories
+{
+    public interface IDomainsRepository<T>
+    {
+        Task<ActionResult<IEnumerable<T>>> GetAllAsync();
+        Task<ActionResult<T>> GetByIdAsync(int id);
+        Task<T> CreateAsync(T entity);
+        Task<ActionResult<bool>> UpdateAsync(T entity);
+        Task<ActionResult<bool>> DeleteAsync(int id);
+        bool isExist(int id);
+    }
+}
