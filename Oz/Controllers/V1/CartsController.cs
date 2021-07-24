95801f7d85d1ceea82ffb8ceba433988d2b668ed
AkshayHolkar <dhamakk@gmail.com>
2021-07-24 12:52:46 +1000

    refactor: implement cart repository in cartsController

--- Oz/Controllers/V1/CartsController.cs
+++ Oz/Controllers/V1/CartsController.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Oz.Data;
-using Oz.Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Oz.Extensions;
 using Oz.Services;
 using Oz.Dtos;
+using Oz.Repositories;
 
 namespace Oz.Controllers.V1
 {
@@ -20,12 +16,12 @@ namespace Oz.Controllers.V1
     [Route("api/v1/Carts")]
     public class CartsController : ControllerBase
     {
-        private readonly DataContext _context;
+        private readonly ICartRepository _repository;
         private readonly ISharedService _sharedService;
 
-        public CartsController(DataContext context, ISharedService sharedService)
+        public CartsController(ICartRepository repository, ISharedService sharedService)
         {
-            _context = context;
+            _repository = repository;
             _sharedService = sharedService;
         }
 
@@ -33,60 +29,49 @@ namespace Oz.Controllers.V1
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CartDto>>> GetCarts()
         {
-            return await _context.Carts.Where(i => i.CustomerId == HttpContext.GetUserId()).Select(cart => cart.AsDto()).ToListAsync();
-
+            var userId = HttpContext.GetUserId();
+            return await _repository.GetAllAsync(userId);
         }
 
         // GET: api/v1/Carts/5
         [HttpGet("{id}")]
         public async Task<ActionResult<CartDto>> GetCart(int id)
         {
-            var cart = await _context.Carts.FindAsync(id);
-
-            if (cart == null)
+            if (!_repository.IsExist(id))
             {
                 return NotFound();
             }
+            var cart = await _repository.GetByIdAsync(id);
 
-            if (!_sharedService.UserOwnsDomain(cart.CustomerId, HttpContext.GetUserId()))
+            if (!_sharedService.UserOwnsDomain(cart.Value.CustomerId, HttpContext.GetUserId()))
             {
                 return BadRequest(new { error = "You do not own this cart" });
             }
 
-            return cart.AsDto();
+            return cart;
         }
 
         // PUT: api/v1/Carts/5
         [HttpPut("{id}")]
         public async Task<IActionResult> PutCart(int id, [FromBody] CartDto cartDto)
         {
+
             if (id != cartDto.Id)
             {
                 return BadRequest();
             }
 
-            if (!_sharedService.UserOwnsDomain(cartDto.CustomerId, HttpContext.GetUserId()))
+            if (!_repository.IsExist(cartDto.Id))
             {
-                return BadRequest(new { error = "You do not own this cart" });
+                return NotFound();
             }
 
-            _context.Entry(cartDto.AsCartFromCartDto()).State = EntityState.Modified;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.ErrorCount);
+            }
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CartExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            await _repository.UpdateAsync(cartDto);
 
             return NoContent();
         }
@@ -95,39 +80,37 @@ namespace Oz.Controllers.V1
         [HttpPost]
         public async Task<ActionResult<CartDto>> PostCart([FromBody] CreateCartDto createCartDto)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             var cart = createCartDto.AsCartFromCreateCartDto();
             cart.CustomerId = HttpContext.GetUserId();
 
-            _context.Carts.Add(cart);
-            await _context.SaveChangesAsync();
+            var cartDto = await _repository.CreateAsync(cart);
 
-            return CreatedAtAction(nameof(GetCart), new { id = cart.Id }, cart.AsDto());
+            return CreatedAtAction(nameof(GetCart), new { id = cart.Id }, cartDto);
         }
 
         // DELETE: api/v1/Carts/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteCart(int id)
         {
-            var cart = await _context.Carts.FindAsync(id);
+            var cart = await _repository.GetByIdAsync(id);
             if (cart == null)
             {
                 return NotFound();
             }
 
-            if (!_sharedService.UserOwnsDomain(cart.CustomerId, HttpContext.GetUserId()))
+            if (!_sharedService.UserOwnsDomain(cart.Value.CustomerId, HttpContext.GetUserId()))
             {
                 return BadRequest(new { error = "You do not own this cart" });
             }
 
-            _context.Carts.Remove(cart);
-            await _context.SaveChangesAsync();
+            await _repository.DeleteAsync(id);
 
             return NoContent();
-        }
-
-        private bool CartExists(int id)
-        {
-            return _context.Carts.Any(e => e.Id == id);
         }
     }
 }
